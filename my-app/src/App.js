46127fb40514d91f73ff1606de2ef93462986592
Senthilkumar-Frontend <senthilkumar.1706045@gmail.com>
2023-11-14 22:30:16 +0530

    Changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,15 @@
-// import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
-import Fleet from './fleet/Fleet';
-// import HotelBooking from './hotelbooking/HotelBooking';
-// import SinglePreview from './hotelbooking/components/preview/SinglePreview';
+import HotelBooking from './hotelbooking/HotelBooking';
+import SinglePreview from './hotelbooking/components/preview/SinglePreview';
 
 export default function App() {
   return (
     <div className="App">
-
-      {/* Fleet studio Interview Question */}
-      <Fleet />
-
-
-      {/* <Routes>
+      <Routes>
         <Route path='/' element={<HotelBooking />}/>
         <Route path='/hotel/:id' element={<SinglePreview />} />
-      </Routes> */}
+      </Routes>
     </div>
   );
 }
--- my-app/src/hotelbooking/HotelBooking.js
+++ my-app/src/hotelbooking/HotelBooking.js
@@ -1,4 +1,6 @@
-import { useState, useEffect, useMemo, createContext } from 'react';
+import axios from "axios";
+
+import { useState, useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import './HotelBooking.css';
@@ -6,7 +8,7 @@ import Header from './components/header/Header';
 import Input from './components/input/Input';
 import Preview from './components/preview/Preview';
 
-export const TestContext = createContext();
+// export const TestContext = createContext();
 
 export default function HotelBooking() {
 
@@ -24,8 +26,8 @@ export default function HotelBooking() {
   useEffect(() => {
     let getHotelList = async () => {
       try {
-        const response = await fetch('https://mocki.io/v1/4775a500-cf31-4bee-8a65-0c849b6e4d0c')
-        const result = await response.json();
+        const response = await axios.get('https://mocki.io/v1/4775a500-cf31-4bee-8a65-0c849b6e4d0c')
+        const result = await response.data;
         setHotelList(result);
       } catch(err) {
         console.log(err)
@@ -44,21 +46,19 @@ export default function HotelBooking() {
   }
 
   return (
-    <TestContext.Provider value={hotelList}>
-      <div className="hotelbooking">
-        { !hotelList.length ? (<div className='loader'>Please push back and sit, Your Coffee is preparing...</div>) : (
-        <>
-          <Header content={'Hotel Rooms Booking'} />
-          <section className="section-container">
-            <div className="hotel-search">
-              <Input placeHolder='search hotel...' inputHandler={handleInput} />
-            </div>
-            <div className="hotelpreview-section">
-              { hotelList && <Preview previewData={filteredHotelList} previewClickHandler={hotelPreviewHandling} /> }
-            </div>
-          </section>
-        </>)}
-      </div>
-    </TestContext.Provider>
+    <div className="hotelbooking">
+      { !hotelList.length ? (<div className='loader'>Please push back and sit, Your Coffee is preparing...</div>) : (
+      <>
+        <Header content={'Hotel Rooms Booking'} />
+        <section className="section-container">
+          <div className="hotel-search">
+            <Input placeHolder='search hotel...' inputHandler={handleInput} />
+          </div>
+          <div className="hotelpreview-section">
+            { hotelList && <Preview previewData={filteredHotelList} previewClickHandler={hotelPreviewHandling} /> }
+          </div>
+        </section>
+      </>)}
+    </div>
   );
 }
